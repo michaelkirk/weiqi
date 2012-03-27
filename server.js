e8a6cadefcbc23e78d393d0871e72141a59bb132
Michael Kirk <michael.john.kirk@gmail.com>
2012-03-26 19:34:39 -0700

    tabs to spaces

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ process.title = siteConf.uri.replace(/http:\/\/(www)?/, '');
 
 var airbrake;
 if (siteConf.airbrakeApiKey) {
-	airbrake = require('airbrake').createClient(siteConf.airbrakeApiKey);
+  airbrake = require('airbrake').createClient(siteConf.airbrakeApiKey);
 }
 
 process.addListener('uncaughtException', function (err, stack) {
-	console.log('Caught exception: '+err+'\n'+err.stack);
-	console.log('\u0007'); // Terminal bell
-	if (airbrake) { airbrake.notify(err); }
+  console.log('Caught exception: '+err+'\n'+err.stack);
+  console.log('\u0007'); // Terminal bell
+  if (airbrake) { airbrake.notify(err); }
 });
 
 var connect = require('connect');
@@ -36,152 +36,153 @@ var socketIo = new require('./lib/socket-io-server.js')(app, sessionStore);
 var authentication = new require('./lib/authentication.js')(app, siteConf);
 // Setup groups for CSS / JS assets
 var assetsSettings = {
-	'js': {
-		'route': /\/static\/js\/[a-z0-9]+\/.*\.js/
-		, 'path': './public/js/'
-		, 'dataType': 'javascript'
-		, 'files': [
-			'http://code.jquery.com/jquery-latest.js'
-			, siteConf.uri+'/socket.io/socket.io.js' // special case since the socket.io module serves its own js
-			, 'jquery.client.js'
-		]
-		, 'debug': true
-		, 'postManipulate': {
-			'^': [
-				assetHandler.uglifyJsOptimize
-				, function insertSocketIoPort(file, path, index, isLast, callback) {
-					callback(file.replace(/.#socketIoPort#./, siteConf.port));
-				}
-			]
-		}
-	}
-	, 'css': {
-		'route': /\/static\/css\/[a-z0-9]+\/.*\.css/
-		, 'path': './public/css/'
-		, 'dataType': 'css'
-		, 'files': [
-			'reset.css'
-			, 'client.css'
-		]
-		, 'debug': true
-		, 'postManipulate': {
-			'^': [
-				assetHandler.fixVendorPrefixes
-				, assetHandler.fixGradients
-				, assetHandler.replaceImageRefToBase64(__dirname+'/public')
-				, assetHandler.yuiCssOptimize
-			]
-		}
-	}
+  'js': {
+    'route': /\/static\/js\/[a-z0-9]+\/.*\.js/
+    , 'path': './public/js/'
+    , 'dataType': 'javascript'
+    , 'files': [
+      'http://code.jquery.com/jquery-latest.js'
+      , siteConf.uri+'/socket.io/socket.io.js' // special case since the socket.io module serves its own js
+      , 'jquery.client.js'
+
+    ]
+    , 'debug': true
+    , 'postManipulate': {
+      '^': [
+        assetHandler.uglifyJsOptimize
+        , function insertSocketIoPort(file, path, index, isLast, callback) {
+          callback(file.replace(/.#socketIoPort#./, siteConf.port));
+        }
+      ]
+    }
+  }
+  , 'css': {
+    'route': /\/static\/css\/[a-z0-9]+\/.*\.css/
+    , 'path': './public/css/'
+    , 'dataType': 'css'
+    , 'files': [
+      'reset.css'
+      , 'client.css'
+    ]
+    , 'debug': true
+    , 'postManipulate': {
+      '^': [
+        assetHandler.fixVendorPrefixes
+        , assetHandler.fixGradients
+        , assetHandler.replaceImageRefToBase64(__dirname+'/public')
+        , assetHandler.yuiCssOptimize
+      ]
+    }
+  }
 };
 // Add auto reload for CSS/JS/templates when in development
 app.configure('development', function(){
-	assetsSettings.js.files.push('jquery.frontend-development.js');
-	assetsSettings.css.files.push('frontend-development.css');
-	[['js', 'updatedContent'], ['css', 'updatedCss']].forEach(function(group) {
-		assetsSettings[group[0]].postManipulate['^'].push(function triggerUpdate(file, path, index, isLast, callback) {
-			callback(file);
-			dummyHelpers[group[1]]();
-		});
-	});
+  assetsSettings.js.files.push('jquery.frontend-development.js');
+  assetsSettings.css.files.push('frontend-development.css');
+  [['js', 'updatedContent'], ['css', 'updatedCss']].forEach(function(group) {
+    assetsSettings[group[0]].postManipulate['^'].push(function triggerUpdate(file, path, index, isLast, callback) {
+      callback(file);
+      dummyHelpers[group[1]]();
+    });
+  });
 });
 
 var assetsMiddleware = assetManager(assetsSettings);
 
 // Settings
 app.configure(function() {
-	app.set('view engine', 'ejs');
-	app.set('views', __dirname+'/views');
+  app.set('view engine', 'ejs');
+  app.set('views', __dirname+'/views');
 });
 
 // Middleware
 app.configure(function() {
-	app.use(express.bodyParser());
-	app.use(express.cookieParser());
-	app.use(assetsMiddleware);
-	app.use(express.session({
-		'store': sessionStore
-		, 'secret': siteConf.sessionSecret
-	}));
-	app.use(express.logger({format: ':response-time ms - :date - :req[x-real-ip] - :method :url :user-agent / :referrer'}));
-	app.use(authentication.middleware.auth());
-	app.use(authentication.middleware.normalizeUserData());
-	app.use(express['static'](__dirname+'/public', {maxAge: 86400000}));
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(assetsMiddleware);
+  app.use(express.session({
+    'store': sessionStore
+    , 'secret': siteConf.sessionSecret
+  }));
+  app.use(express.logger({format: ':response-time ms - :date - :req[x-real-ip] - :method :url :user-agent / :referrer'}));
+  app.use(authentication.middleware.auth());
+  app.use(authentication.middleware.normalizeUserData());
+  app.use(express['static'](__dirname+'/public', {maxAge: 86400000}));
 
-	// Send notification to computer/phone @ visit. Good to use for specific events or low traffic sites.
-	if (siteConf.notifoAuth) {
-		app.use(notifoMiddleware(siteConf.notifoAuth, {
-			'filter': function(req, res, callback) {
-				callback(null, (!req.xhr && !(req.headers['x-real-ip'] || req.connection.remoteAddress).match(/192.168./)));
-			}
-			, 'format': function(req, res, callback) {
-				callback(null, {
-					'title': ':req[x-real-ip]/:remote-addr @ :req[host]'
-					, 'message': ':response-time ms - :date - :req[x-real-ip]/:remote-addr - :method :user-agent / :referrer'
-				});
-			}
-		}));
-	}
+  // Send notification to computer/phone @ visit. Good to use for specific events or low traffic sites.
+  if (siteConf.notifoAuth) {
+    app.use(notifoMiddleware(siteConf.notifoAuth, {
+      'filter': function(req, res, callback) {
+        callback(null, (!req.xhr && !(req.headers['x-real-ip'] || req.connection.remoteAddress).match(/192.168./)));
+      }
+      , 'format': function(req, res, callback) {
+        callback(null, {
+          'title': ':req[x-real-ip]/:remote-addr @ :req[host]'
+          , 'message': ':response-time ms - :date - :req[x-real-ip]/:remote-addr - :method :user-agent / :referrer'
+        });
+      }
+    }));
+  }
 });
 
 // ENV based configuration
 
 // Show all errors and keep search engines out using robots.txt
 app.configure('development', function(){
-	app.use(express.errorHandler({
-		'dumpExceptions': true
-		, 'showStack': true
-	}));
-	app.all('/robots.txt', function(req,res) {
-		res.send('User-agent: *\nDisallow: /', {'Content-Type': 'text/plain'});
-	});
+  app.use(express.errorHandler({
+    'dumpExceptions': true
+    , 'showStack': true
+  }));
+  app.all('/robots.txt', function(req,res) {
+    res.send('User-agent: *\nDisallow: /', {'Content-Type': 'text/plain'});
+  });
 });
 // Suppress errors, allow all search engines
 app.configure('production', function(){
-	app.use(express.errorHandler());
-	app.all('/robots.txt', function(req,res) {
-		res.send('User-agent: *', {'Content-Type': 'text/plain'});
-	});
+  app.use(express.errorHandler());
+  app.all('/robots.txt', function(req,res) {
+    res.send('User-agent: *', {'Content-Type': 'text/plain'});
+  });
 });
 
 // Template helpers
 app.dynamicHelpers({
-	'assetsCacheHashes': function(req, res) {
-		return assetsMiddleware.cacheHashes;
-	}
-	, 'session': function(req, res) {
-		return req.session;
-	}
+  'assetsCacheHashes': function(req, res) {
+    return assetsMiddleware.cacheHashes;
+  }
+  , 'session': function(req, res) {
+    return req.session;
+  }
 });
 
 // Error handling
 app.error(function(err, req, res, next){
-	// Log the error to Airbreak if available, good for backtracking.
-	console.log(err);
-	if (airbrake) { airbrake.notify(err); }
+  // Log the error to Airbreak if available, good for backtracking.
+  console.log(err);
+  if (airbrake) { airbrake.notify(err); }
 
-	if (err instanceof NotFound) {
-		res.render('errors/404');
-	} else {
-		res.render('errors/500');
-	}
+  if (err instanceof NotFound) {
+    res.render('errors/404');
+  } else {
+    res.render('errors/500');
+  }
 });
 function NotFound(msg){
-	this.name = 'NotFound';
-	Error.call(this, msg);
-	Error.captureStackTrace(this, arguments.callee);
+  this.name = 'NotFound';
+  Error.call(this, msg);
+  Error.captureStackTrace(this, arguments.callee);
 }
 
 // Routing
 app.all('/', function(req, res) {
-	// Set example session uid for use with socket.io.
-	if (!req.session.uid) {
-		req.session.uid = (0 | Math.random()*1000000);
-	}
-	res.locals({
-		'key': 'value'
-	});
-	res.render('index');
+  // Set example session uid for use with socket.io.
+  if (!req.session.uid) {
+    req.session.uid = (0 | Math.random()*1000000);
+  }
+  res.locals({
+    'key': 'value'
+  });
+  res.render('index');
 });
 
 // Initiate this after all other routing is done, otherwise wildcard will go crazy.
@@ -189,7 +190,7 @@ var dummyHelpers = new DummyHelper(app);
 
 // If all fails, hit em with the 404
 app.all('*', function(req, res){
-	throw new NotFound;
+  throw new NotFound;
 });
 
 console.log('Running in '+(process.env.NODE_ENV || 'development')+' mode @ '+siteConf.uri);
