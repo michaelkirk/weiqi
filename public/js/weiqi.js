9a3566b37658c73b93fffb6385c32b7f24f12616
Sam Vevang <sam.vevang@gmail.com>
2012-04-21 14:15:24 -0500

    remove stray space

--- public/js/weiqi.js
+++ public/js/weiqi.js
@@ -1,5 +1,5 @@
 // In case we leave a console.*** in the code without native support
-(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info, log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c;})(window.console=window.console||{});
+(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c;})(window.console=window.console||{});
 
 var _init = function (weiqi) {
 
