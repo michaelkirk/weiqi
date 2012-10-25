c91d566bfe2d88d8780993dc561a932e4161c90d
Sam Vevang <sam.vevang@gmail.com>
2012-10-24 21:30:09 -0400

    remove debugger call

--- app.js
+++ app.js
@@ -62,7 +62,6 @@ var routes = require('./routes')(app);
 var server = http.createServer(app);
 var io = require('socket.io').listen(server)
 
-debugger
 // bind to a port
 server.listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
