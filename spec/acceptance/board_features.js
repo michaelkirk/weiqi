242581de7fc930cd62085b6718f4a7a64dce3c2f
Michael Kirk <michael.john.kirk@gmail.com>
2012-11-01 15:04:18 +1100

    working test suite (with a consequent bug fix in CellView!)

---
+++ spec/acceptance/board_features.js
@@ -0,0 +1,79 @@
+Zombie = require('zombie');
+assert = require('assert');
+
+//TODO set root site path (site: "localhost:3000")
+browser = new Zombie();
+browser.on("error", function(error) {
+  report("ERROR> " + error);
+})
+
+function report(msg) {
+  console.log("test> " + msg);
+}
+
+function run_suite() {
+  report('Starting acceptance suite...');
+  test_server_up()
+  .then(test_boards)
+  .then(function() {
+    report("SUCCESS! ^_^ GOOD JOB!");
+    process.exit(0);
+  }).fail(function(error) {
+    report("FAILED with: ``" + error + "''");
+    process.exit(1);
+  });
+}
+
+function test_server_up(){
+  return browser
+    .visit("http://localhost:3000")
+    .then(function() {
+      assert.ok(browser.success);
+    })
+    .fail(function(){
+      report("The server must be up in order to run the suite.");
+    });
+}
+
+function test_boards( ){
+  return browser
+    .visit("http://localhost:3000/boards")
+    .then(function(){ 
+      report("successfully loaded board creation button");
+      return browser.pressButton("start a new game"); 
+    })
+    .then(function(){
+      assert.ok(browser.success);
+      assert.ok(browser.redirected);
+      assert.ok(browser.location.pathname.match(/^\/boards\/[0-9]+$/));
+      report('successfully redirected to new board.');
+      var board_id = browser.location.pathname.match(/^\/boards\/([0-9]+)$/)[1];
+      return board_id
+    })
+    .then(function(board_id) { return browser.visit("http://localhost:3000/boards/" + board_id); })
+    .then(function() {
+      assert.ok(browser.query('#black .board .jgo_c:nth-child(24)'));
+      report('successfully rendered new board.');
+    })
+    .then(function() { 
+      debugger
+      var cell_element = browser.querySelector('#black .board .jgo_c:nth-child(24)');
+      return browser.fire('click', cell_element);
+    })
+    .then(function() {
+      assert.ok(browser.query('#black .board .jgo_c:nth-child(24).jgo_b'));
+      report('successfully placed a stone.');
+      var board_id = browser.location.pathname.match(/^\/boards\/([0-9]+)$/)[1];
+      return board_id;
+    })
+    .then(function(board_id) {
+      return browser.visit("http://localhost:3000/boards/" + board_id);
+    })
+    .then(function(){
+      assert.ok(browser.query('#black .board .jgo_c:nth-child(24).jgo_b'));
+      report('successfully saved and reloaded the board intact');
+    });
+};
+
+run_suite();
+
