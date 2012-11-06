2d23bf61b322219774d2f2bf4144844e6abe95be
Sam Vevang <sam.vevang@gmail.com>
2012-11-06 08:35:20 -0400

    update controller code to use new persistence logic

--- controllers/boards.js
+++ controllers/boards.js
@@ -12,59 +12,58 @@ module.exports = function(app){
   boards.show = function(req, res){
 
     var board = new weiqi.Board({id: req.params.id})
-    board.fetch().then(
-    // success  
-    function(){
-      if(req.params.format == 'json') {
-        redis_client.get('boards:' + req.params.id, function(err, result) {
+    board.fetch()
+      .then(function(){
+        if(req.params.format == 'json') {
           res.set('Content-Type', 'application/json');
           res.send(board.toJSON());
-        });
-      } else {
-        redis_client.get('boards:' + req.params.id, function(err, result) {
+        } else {
           res.render('boards/show', {
             id: req.params.id,
             board_json: JSON.stringify(board.toJSON())
           });
-        });
-      }
-    },
-    // Failure
-    function(){
+        }
+      })
+    .fail(function(){
       res.status(404);
       res.render('errors/404');
-    })
+    });
 
   }// end boards.show
 
   boards.create = function(req, res){
     var board = new weiqi.Board()
-    promise = board.save()
-    promise.then(function(){
-      res.redirect(302, '/boards/' + board.id);
-    },
-    function(err){
-      res.send("Error");
-    }
-    )
+    board.save()
+      .then(function(){
+        res.redirect(302, '/boards/' + board.id);
+      })
+      .fail(function(err){
+        res.send("Error");
+      });
   }
 
   boards.update = function(req, res){
-    var id = req.params.id
-
-    //TODO see if it exists, else 404
-    if(req.params.format == 'json') {
-      attributes_string = JSON.stringify(req.body);
-      redis_client.set('boards:' + id, attributes_string, function(err, result) {
+    var board = new weiqi.Board({id: req.params.id});
+    board.fetch()
+      .then(function(){
+        board.set(req.body)
+        return board.save()
+      })
+      .then(function(){
         app.io.sockets.emit('board-update');
-        res.status(200);
-        res.set('Content-Type', 'application/json');
-        res.send(attributes_string);
-      });
-    } else {
-      res.status(404);
-      res.render('errors/404');
-    }
+        if(req.params.format == 'json') {
+          attributes_string = JSON.stringify(board.toJSON());
+          res.status(200);
+          res.set('Content-Type', 'application/json');
+          res.send(attributes_string);
+        } else {
+          res.status(404);
+          res.render('errors/404');
+        }
+      }).fail(function(err){
+          res.status(500);
+          res.render('errors/500');
+      })
   }
 
   return boards;
