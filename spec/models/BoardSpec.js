9cb4a01a24be6face2c0f2bce84bb6dc114a345a
Sam Vevang <sam.vevang@gmail.com>
2012-11-24 14:04:02 -0400

    new jasmine specs for moves

--- spec/models/BoardSpec.js
+++ spec/models/BoardSpec.js
@@ -141,6 +141,36 @@ describe("Board", function() {
         expect(board.get_cell(0, 0).is_empty()).toBe(true);
       });
     });
+    describe('moves', function(){
+      it("should accumulate a list of moves", function(){
+        board.play_black(0, 1);
+        board.play_white(10, 12);
+        board.play_black(1, 0);
+        board.play_white(0, 0);
+        expect(board.moves.length).toBe(4)
+ 
+      });
+      it("should order the moves in the way they were played", function(){
+        var moves = [
+          {color:'black', x:0,  y:1}, 
+          {color:'white', x:10, y:12}, 
+          {color:'black', x:1,  y:0}, 
+          {color:'white', x:0,  y:0} 
+        ];
+        moves = _(moves).map(function(move_data){
+          return new weiqi.Move(move_data);
+        });
+        _(moves).each(function(move){
+          move.apply_to(board)
+        });
+        expect(moves.length).toBe(board.moves.length);
+        for(i=0; i<moves.length; i++){
+          expect(moves[i].get('x')).toBe(board.moves.models[i].get('x'));
+          expect(moves[i].get('y')).toBe(board.moves.models[i].get('y'));
+          expect(moves[i].get('color')).toBe(board.moves.models[i].get('color'));
+        };
+      });
+    });
   });
 
   describe("#stone_cell_groups", function() {
