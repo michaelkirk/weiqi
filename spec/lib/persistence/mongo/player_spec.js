5dee1541c24a4323b0691c3889a6f1ed57da2494
Michael Kirk <michael.john.kirk@gmail.com>
2015-02-04 11:22:08 -0800

    organize player test

--- spec/lib/persistence/mongo/player_spec.js
+++ spec/lib/persistence/mongo/player_spec.js
@@ -3,22 +3,21 @@ var Player = require('../../../../lib/weiqi-models').Player;
 var mongo_promise = require('../../../../lib/persistence/mongo/client.js');
 
 describe("Player", function() {
+  var mock_board_id = "fake-boarduid-----------"
 
+  //clean up any created test data
   beforeEach(function(done){
-    Player.find_by_board_id_and_color('fake-boarduid-----------', 'black').then(function(player){
+    Player.find_by_board_id_and_color(mock_board_id, 'black').then(function(player){
       return player.delete();
     }).then(function(){
-      done(); 
-    }).fail(function(err){
-      done(err);
+      done();
+    }).fail(function(error){
+      done(error);
     })
-
   })
 
   describe("#create", function() {
-    it('should be possible to create and fetch a Player', function(done) {
-      var mock_board_id = "fake-boarduid-----------"
-
+    it('is possible to fetch a created Player', function(done) {
       var player = new Player({ board_id: mock_board_id, color: "black" });
       player.save().then(function() {
         return player.fetch()
@@ -30,9 +29,10 @@ describe("Player", function() {
         console.log("failure:" + error);
       });
     });
+  });
 
-    it("should be able to lookup by board id and color", function(done) {
-      var mock_board_id = "fake-boarduid-----------"
+  describe(".find_by_board_id_and_color", function() {
+    it("finds an existing player", function(done) {
       var player = new Player({ board_id: mock_board_id, color: "black" });
       var found_player;
       player.save().then(function(saved_attributes) {
@@ -45,8 +45,8 @@ describe("Player", function() {
         assert.equal(fetched_attributes._id, player.id, "didn't find the right player");
         done();
       }).fail(function(error) {
+        console.log("failure:" + error);
       });
     });
   });
-
 });
